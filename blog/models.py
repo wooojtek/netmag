a5eb655988213c9f5558ef8a866d6f1e4fe91cf3
wooojtek <wbabinski@gmail.com>
2014-02-11 19:34:40 +0000

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
+    description = models.CharField(max_length=255)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField('Category', related_name='posts')
+ 
+    class Meta:
+        ordering = ['-created']
+
+    def __unicode__(self):
+        return u'%s' % self.title
+ 
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args=[self.slug])
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+    #posts = models.ManyToManyField(Post, related_name='categories')
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __unicode__(self):
+        return u'%s' % self.category_name
---
+++ netmag/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
+    url(r'^blog/comments/', include('fluent_comments.urls')),
+)
